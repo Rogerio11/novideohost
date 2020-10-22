711683b3735eb53adf3a048bdf4d884f759654e7
Rogerio11 <55982331+Rogerio11@users.noreply.github.com>
2020-10-22 22:44:10 +0200

    debug true

--- novideohost/settings.py
+++ novideohost/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h85kql$#t*s4rm#5__2+e-pb1ij%m1gey91e2g!swmw9mz^@^*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['novideohost.herokuapp.com']
 
