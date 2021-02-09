512fc57f146579faa87137ca4afdd765a3c14675
Rogerio11 <55982331+Rogerio11@users.noreply.github.com>
2021-02-09 20:57:55 +0100

    404 error rr

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app.views import addTuto, tutoPage, updateTuto, upd_del_tuto,connexion,listTutos,pour1404, pour2404
+from app.views import addTuto, tutoPage, updateTuto, upd_del_tuto,connexion,listTutos
 urlpatterns = [
     path("", connexion, name="connexion"),
     path("list", listTutos, name="list"),
@@ -7,6 +7,4 @@ urlpatterns = [
     path("tuto/<str:idTuto>/update", updateTuto, name = "update"),
     path("tuto/<str:idTuto>/upd-del", upd_del_tuto, name = "updatedeletetuto"),
     path("tuto/<str:idTuto>",tutoPage, name="tuto"),
-    path("<str:param1>",pour1404, name="pour404"),
-    path("<str:param1>/<str:param2>",pour2404, name="pour404"),
 ]
